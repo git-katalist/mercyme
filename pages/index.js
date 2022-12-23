7be24a8bbe4c23157db88c54badef03de9a2ff9a
tsots <mosalatsotetsi@gmail.com>
2022-12-23 11:28:14 +0200

    run export and remove Image instance from index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import Meta from "../components/seo/Meta";
 import { prefix } from "../utils/prefix";
 
 export default function Home() {
