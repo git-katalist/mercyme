9646132ff724a6866056fa0a336db445e869d666
git-katalist <79943903+git-katalist@users.noreply.github.com>
2022-12-24 00:20:48 +0000

    Deploying to gh-pages from @ git-katalist/mercyme@3b782fdac920985d2252a9043e3e309561423136 🚀

---
+++ _next/static/chunks/pages/contact-43056f34a7ed0755.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[335],{8367:function(n,t,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/contact",function(){return c(8926)}])},8926:function(n,t,c){"use strict";c.r(t);var u=c(5893);let _=()=>(0,u.jsx)("div",{children:"contact"});t.default=_}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8367)}),_N_E=n.O()}]);
