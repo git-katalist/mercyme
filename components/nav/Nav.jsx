abe2f615fafef2980d6d55d689a7374a9664e260
tsots <mosalatsotetsi@gmail.com>
2022-12-24 02:44:06 +0200

    changed Image import actins

--- components/nav/Nav.jsx
+++ components/nav/Nav.jsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { StyledNav, LogoWrapper, Logo, Links } from "./StyledNav";
-const flavicon = "/favicon.ico";
+import { favicon } from "../../public/index";
 import { prefix } from "../../utils/prefix";
 
 const Nav = () => (
   <StyledNav>
     <LogoWrapper>
-      <Logo src={prefix + flavicon} alt="" />
+      <Logo src={prefix + favicon.src} alt="" />
+      {/* <img src={prefix + favicon.src} alt="" /> */}
       {/* <Logo /> */}
     </LogoWrapper>
     <Links>
